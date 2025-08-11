f3bf2f91376eb32b4cacc307b88d08e2714758ea
nunonuno7 <121554477+nunonuno7@users.noreply.github.com>
2025-08-11 13:35:31 +0100

    feat: implement activation email utility

--- core/utils/email_helpers.py
+++ core/utils/email_helpers.py
@@ -4,10 +4,14 @@ Email helper utilities for ourfinancetracker
 """
 
 import logging
-from django.core.mail import send_mail, EmailMessage
-from django.template.loader import render_to_string
 from django.conf import settings
-from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from accounts.tokens import account_activation_token
 
 logger = logging.getLogger(__name__)
 
@@ -59,12 +63,42 @@ def send_template_email(subject, template_name, context, recipient_list,
 
 
 def send_account_activation_email(user, request):
+    """Send account activation email to ``user``.
+
+    The email includes a unique activation link based on a token
+    generated for the user. The link is built using ``request`` so it
+    contains the correct domain and protocol.
+
+    Args:
+        user (django.contrib.auth.models.User): The user to activate.
+        request (django.http.HttpRequest): Current request instance used
+            to build the absolute activation URL.
+
+    Returns:
+        bool: ``True`` if the email was sent successfully, otherwise
+        ``False``.
     """
-    Send account activation email to user.
-    This is a placeholder for future implementation.
-    """
-    # TODO: Implement account activation functionality
-    pass
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = account_activation_token.make_token(user)
+    activation_link = request.build_absolute_uri(
+        reverse("accounts:activate", kwargs={"uidb64": uid, "token": token})
+    )
+
+    context = {"user": user, "activation_link": activation_link}
+
+    try:
+        send_mail(
+            "Activate your account",
+            render_to_string("accounts/emails/account_activation_email.txt", context),
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            fail_silently=False,
+        )
+        logger.info("Activation email sent to %s", user.email)
+        return True
+    except Exception as exc:
+        logger.error("Failed to send activation email to %s: %s", user.email, exc)
+        return False
 
 
 def test_email_configuration():
